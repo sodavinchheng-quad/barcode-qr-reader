193663f9441d9f415929af52e05ec88401e31211
sodavinchheng-quad <sodavin.chheng@quad.co.jp>
2024-03-22 21:59:11 +0900

    Initial update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import { BrowserMultiFormatReader } from "@zxing/library";
+import Webcam from "react-webcam";
 
 function App() {
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
+  const onUpdate = (err: any, res?: any) => {
+    if (res) setResult(res.text);
+    else setResult("no data")
+    if (err) setError(JSON.stringify(err));
+  };
+  const webcamRef = useRef(null);
+  const codeReader = new BrowserMultiFormatReader();
+  const capture = useCallback(() => {
+    let _a;
+    const imageSrc =
+      (_a =
+        webcamRef === null || webcamRef === void 0
+          ? void 0
+          : webcamRef.current) === null || _a === void 0
+        ? void 0
+        // @ts-ignore
+        : _a.getScreenshot();
+    if (imageSrc) {
+      codeReader
+        .decodeFromImage(undefined, imageSrc)
+        .then((result) => {
+          onUpdate(null, result);
+        })
+        .catch((err) => {
+          onUpdate(err);
+        });
+    }
+  }, [codeReader, onUpdate]);
+
+  useEffect(() => {
+    setInterval(capture, 500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Webcam
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{
+          facingMode: "environment",
+        }}
+      />
+      <p>Result: {result}</p>
+      <p>Error: {error}</p>
+    </>
   );
 }
 
